72ce98fd28aa23891275bbe251189fe1edb92d60
mariusz <mfornal@biostat.com.pl>
2024-07-09 22:40:20 +0200

    first test

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ## What we do here?
 here is step by step - commit after commit show of TDD development.
 Red - Green - Refactor.
+
+# 1. Step: Write the minimal breaking test.
---
+++ tests/Application/DrugCalculatorControllerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Tests\Application;
+
+use PHPUnit\Framework\Attributes\Test;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class DrugCalculatorControllerTest extends WebTestCase
+{
+    const string DRUG_CALCULATOR_CALCULATE_DOSE_ENDPOINT = '/drug-calculator/calculate-dose';
+    private KernelBrowser $httpClient;
+
+    public function setUp(): void
+    {
+        $this->httpClient = static::createClient([], ['HTTP_Content-Type' => 'application/json']);
+    }
+    #[Test]
+    public function testCalculateDose()
+    {
+        $this->httpClient->request(
+            'POST',
+            self::DRUG_CALCULATOR_CALCULATE_DOSE_ENDPOINT,
+        );
+
+        $this->assertEquals(Response::HTTP_OK, $this->httpClient->getResponse()->getStatusCode());
+    }
+}
